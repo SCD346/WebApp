ada0e282cf2605495782e5c8220547e00d0a46ea
steve_D <scd346@nyu.edu>
2020-09-08 14:04:32 -0700

    Updated db.js
    Updated db.js comments

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
+//THIS FILE SETS UP THE CONNECTION TO POSTGRESQL
+        //DB created, sitting on local host 5432
 const Pool = require("pg").Pool; //PG library enables connection to the database with server
 
 // Client object (pool)
